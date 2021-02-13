909920b5329731e399765f2d7398b1d10af56cc3
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-13 19:53:50 -0300

    Página Welcome, com form de registro e link de login.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>My Transfer -</title>
+    <title>Send-me: Envie e receba com precisão, segurança e facilidade!</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
     <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
