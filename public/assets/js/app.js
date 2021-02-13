7ba4d059bd1f14bb06dca5260247012e76570c92
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-13 19:30:13 -0300

    Adicionado template básico, para home, login e registro.

---
+++ public/assets/js/app.js
@@ -0,0 +1,103 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 71)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Scroll to top button appear
+    $(document).scroll(function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+})(jQuery); // End of use strict
+
+$("#isPerfilLojista").click(function(event){
+    var isChecked = $(this).prop("checked");
+    var doc_cpf = $("#doc_cpf");
+    var doc_cnpj = $("#doc_cnpj");
+
+    if(isChecked){
+        doc_cnpj.show(300);
+        doc_cpf.hide();
+    }
+    else {
+        doc_cnpj.hide();
+        doc_cpf.show(300);
+    }
+
+});
+
+$("#formCadastro").on("submit", function(event){
+    var form = document.getElementById("formCadastro");
+
+    if($("#isPerfilLojista").prop("checked")){
+        $("#documento_cnpj").prop("required", true);
+        $("#documento_cpf").prop("required", false);
+    }
+    else {
+        $("#documento_cnpj").prop("required", false);
+        $("#documento_cpf").prop("required", true);
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (form.checkValidity())
+    {
+
+    }
+
+    $(this).addClass('was-validated');
+
+});
---
+++ resources/views/painel/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>My Transfer -</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="/assets/css/login.css">
+</head>
+<body>
+    <div class="wrapper fadeInDown">
+
+        <center><a href="/"><small>Voltar para o início</small></a></center>
+        <div id="formContent" class="mt-2">
+            <!-- Icon -->
+            <div class="fadeIn first p-3">
+                <h5>Identifique-se</h5>
+            </div>
+
+            <!-- Login Form -->
+            <form>
+                <input type="text" id="email" class="fadeIn second" name="email" placeholder="E-mail">
+                <input type="text" id="senha" class="fadeIn third" name="senha" placeholder="Senha">
+                <input type="submit" class="fadeIn fourth" value="Entrar">
+            </form>
+
+            <!-- Remind Passowrd -->
+            <div id="formFooter">
+                <a class="underlineHover" href="#">Esqueceu sua senha?</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
