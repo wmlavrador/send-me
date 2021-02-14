31e1a239ce6f6408ed03cdd90153e0c0f0eb2da1
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-14 11:46:06 -0300

    Alterado tipo da coluna "documento", para receber mais caracteres.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('documento')->unique();
+            $table->bigInteger('documento')->unique();
             $table->string('tipo_conta');
             $table->rememberToken();
             $table->timestamps();
