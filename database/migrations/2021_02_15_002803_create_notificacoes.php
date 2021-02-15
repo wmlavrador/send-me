0f4889b467cb63687b483ed82934d6a321c8e778
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-14 23:39:29 -0300

    Migration da tabela de notificações.

---
+++ database/migrations/2021_02_15_002803_create_notificacoes.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificacoes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notificacoes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notificacoes');
+    }
+}
