719feee36a7885ea6104e5ca919df772a342af92
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-16 16:38:32 -0300

    Evento para disparar ao Client a notificação de transação aprovada!

--- app/Http/Controllers/NotificacoesCtr.php
+++ app/Http/Controllers/NotificacoesCtr.php
@@ -2,19 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Events\NovaTransacao;
+use App\Models\Notificacoes;
+use Illuminate\Support\Facades\Http;
 
 class NotificacoesCtr extends Controller
 {
-    public function index(){
-
-    }
 
     public static function novaNotificacao($input = array()){
 
+        $mokyNotify = "https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04";
+        $response = Http::get($mokyNotify);
+
+        if($response['message'] == "Enviado" && $response->successful()){
+            event( new NovaTransacao($input['mensagem'], $input['payee']));
+            $input['status'] = 2;
+        }
+        else {
+            $input['status'] = 1;
+        }
+
+        return Notificacoes::create([
+            "origem" => $input['origem'],
+            "id_origem" => $input['id_origem'],
+            "mensagem" => $input["mensagem"],
+            "situacao" => $input['status'],
+            "user_id" => $input['payee'],
+        ]);
+
     }
 
-    public function updateNotificacao(Request $request){
-
-    }
 }
--- app/Models/Notificacoes.php
+++ app/Models/Notificacoes.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Notificacoes extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "origem",
+        "id_origem",
+        "mensagem",
+        "situacao",
+        "user_id"
+    ];
+
+    public static function getStatus($cod){
+        $status = [
+          "1" => "Em Andamento",
+          "2" => "Enviado",
+          "3" => "Entregue"
+        ];
+
+        return $status[$cod];
+    }
+
+    public static function getOrigem($cod){
+        $origem = [
+            "1" => "Transações",
+        ];
+
+        return $origem[$cod];
+    }
 }
