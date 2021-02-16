88512a23cb25ca67a1625d86b0ebc97111fe951f
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-15 21:34:16 -0300

    Adicionado rotas de transações e carteira.

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserCtr;
 use App\Http\Controllers\TransacoesCtr;
+use \App\Http\Controllers\UserWalletCtr;
 
 Route::prefix("user")->group(function(){
     Route::post("/create", [UserCtr::class, 'store']);
@@ -20,6 +21,14 @@ Route::middleware("auth:sanctum")->group(function(){
     Route::prefix("transacoes")->group(function(){
         Route::get("/listar", [TransacoesCtr::class, 'index']);
         Route::get("/destinatarios", [TransacoesCtr::class, 'destinatarios']);
+
+        Route::post("/estornar/{idTransacao}", [TransacoesCtr::class, "estornar"]);
+        Route::post("/devolver/{idTransacao}", [TransacoesCtr::class, "devolver"]);
+    });
+
+    Route::prefix("/minhas-carteiras")->group(function(){
+        Route::get("/listar", [UserWalletCtr::class, 'index']);
+        Route::post("/depositar/{idCarteira}", [UserWalletCtr::class, 'depositar']);
     });
 
     Route::post("/transaction", [TransacoesCtr::class, 'store']);
