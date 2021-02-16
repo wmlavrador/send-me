c82117a335bc6e3b2134b46cc1a85838f32d464c
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-16 16:32:06 -0300

    Getter com descrição das "situações" da transação.

--- app/Models/Transacoes.php
+++ app/Models/Transacoes.php
@@ -16,4 +16,15 @@ class Transacoes extends Model
         "situacao"
     ];
 
+    public static function getSituacao($cod){
+        $situacoes = [
+          "1" => "Em Andamento",
+          "2" => "Aprovado",
+          "3" => "Estornado",
+          "4" => "Devolvido",
+          "5" => "Não Autorizado",
+        ];
+
+        return $situacoes[$cod];
+    }
 }
