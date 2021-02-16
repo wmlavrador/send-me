64322bf9bd0b039a5704344742f9cdec5185c162
Wilson Lavrador <wilsonmlavrador@gmail.com>
2021-02-15 22:12:48 -0300

    Adicionado listagem de carteira e transações

--- public/assets/js/admin.home.js
+++ public/assets/js/admin.home.js
@@ -15,7 +15,60 @@ $(function(){
 
     // Get Transações.
     $.get("/api/transacoes/listar").then(function(response){
-        console.log();
+        var html = "";
+        $.each(response, function(ind, transacao){
+
+            var acoes = ``;
+
+            html += `
+                <tr>
+                    <th scope="row">`+transacao['id']+`</th>
+                    <td>`+transacao['pagador']+`</td>
+                    <td>`+transacao['recebedor']+`</td>
+                    <td>R$ `+transacao['valor']+`</td>
+                    <td>`+transacao['atualizado']+`</td>
+                    <td>
+                        <div class="dropdown show">
+                          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Ações
+                          </a>
+
+                          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="javascript:void" onclick="devolver(`+transacao['id']+`)">Devolver</a>
+                            <a class="dropdown-item" href="javascript:void">Estornar</a>
+                            <a class="dropdown-item" href="javascript:void">Apagar</a>
+                          </div>
+                        </div>
+                    </td>
+                </tr>
+            `;
+        });
+
+        $("#recebeTransacoes").html(html);
+    });
+
+    // get carteiras
+    $.get("/api/minhas-carteiras/listar").then(function(response){
+       var html = "";
+
+        $.each(response, function(ind, carteira){
+            html += `
+                <div class="card">
+                    <div class="card-header">
+                       `+carteira['descricao']+`
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title"><small>saldo </small><span id="recebeSaldo">R$ `+carteira['saldo']+`</span></h5>
+                        <a href="#" class="btn btn-primary" id="depositar">Depositar</a>
+                        <a href="#" class="btn btn-primary" id="novaTransacao">Transferir</a>
+                    </div>
+                </div>
+            `;
+        });
+
+       $("#recebeCarteiras").html(html);
+    }).fail(function(response){
+
     });
 
     $("#novaTransacao").click(function(){
@@ -23,7 +76,7 @@ $(function(){
             payee: $()
         };
 
-        $.post("/api/transactio", dados).then(function(response){
+        $.post("/api/transaction", dados).then(function(response){
            console.log(response)
         }).fail(function(response){
             console.log( "FAIL" )
@@ -31,5 +84,8 @@ $(function(){
 
     });
 
-
 });
+
+function devolver(id){
+    alert(id);
+}
